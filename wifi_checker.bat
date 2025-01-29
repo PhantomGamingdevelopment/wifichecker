be9058f9805ebdcbfab86653beac149bba752593
Even <84231578+PhantomGamingdevelopment@users.noreply.github.com>
2025-01-29 17:10:13 +0100

    Fix code

--- wifi_checker.bat
+++ wifi_checker.bat
@@ -1,17 +1,14 @@
 @echo off
 
 set IP_adr=8.8.8.8
-
+echo Connected Network: %SSID%
+Title Connected Network: %SSID%
 :loop
 
 for /f "tokens=2 delims=:" %%i in ('netsh wlan show interfaces ^| findstr "SSID"') do (
     set "SSID=%%i"
 )
 set "SSID=%SSID:~1%"
-
-echo Connected Network: %SSID%
-
-
 
 PING -n 1 %IP_adr%> nul
 IF ERRORLEVEL 1 (echo network: %SSID% has lost connection.)
